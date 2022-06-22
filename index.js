2c3c4b81ee0907cb8dce21f4eaa95da9780cfc51
Luciana Roberto <robertolucianabelen@gmail.com>
2022-06-22 11:24:51 -0300

    deploy

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const bp = require('body-parser');
 const qr = require('qrcode');
+const cors = require('cors');
 
 app.set("view engine", "ejs");
 app.use(bp.urlencoded({extended: true}));
 app.use(bp.json());
 app.use( express.static( "views" ) );
+
+
+// posible solución para posible problema de CORS
+const whitelist = ['http://localhost:8080', 'https://myapp.co']; //lista de dominios permitidos EJEMPLOS
+const options = {
+  origin: (origin, callback) => {
+    if (whitelist.includes(origin) || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('no permitido'));
+    }
+  },
+};
+app.use(cors(options));
 
 app.get('/', (req, res) => {
     res.render('index');
